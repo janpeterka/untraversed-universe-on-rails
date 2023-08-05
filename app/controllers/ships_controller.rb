0a604f5845f99c9f7084e32cfb600fb84c973bcc
Jan Peterka <jan.peterka@hey.com>
2023-08-05 18:20:54 +0200

    Add players ship

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -7,12 +7,14 @@ class ShipsController < ApplicationController
     @ship.player = current_player
     @ship.save
 
-    p @ship
-
     if @ship.save
       redirect_to player_path(@ship.player)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @ship = Ship.find(params[:id])
   end
 end
