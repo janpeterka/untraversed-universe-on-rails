186fca7a1f3d28fa98c71ec840e24dfb5fb55af8
Jan Peterka <jan.peterka@hey.com>
2023-08-05 18:43:49 +0200

    Skip onboarding if already done

--- app/controllers/onboarding_controller.rb
+++ app/controllers/onboarding_controller.rb
@@ -4,14 +4,20 @@ class OnboardingController < ApplicationController
   end
 
   def create_player
+    redirect_to onboarding_choose_background_path unless current_player.nil?
+
     @player = Player.new
   end
 
   def choose_background
+    redirect_to onboarding_choose_ship_path unless current_player.qualities.nil?
+
     @player = current_player
   end
 
   def choose_ship
+    redirect_to current_player unless current_player.ship.nil?
+
     @ships = Ship.default_ships
   end
 end
